b66f3961b069678d7cef11d58f48c5b7bb8a2e4f
eduardo sabino <sabino.eduardo.guerra@gmail.com>
2022-11-03 19:30:39 -0300

    first commit

---
+++ src/test.js
@@ -0,0 +1,8 @@
+var Test = /** @class */ (function () {
+    function Test() {
+        this._test = "test";
+    }
+    return Test;
+}());
+var test = new Test();
+console.log(test._test);
---
+++ src/test.ts
@@ -0,0 +1,6 @@
+class Test {
+    public _test: string = "test";
+}
+
+const test = new Test();
+console.log(test._test);
